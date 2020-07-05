76436e7561a8512eec532e503af06c19285ab01d
れみろう <remyroez+github@gmail.com>
2020-07-05 19:59:17 +0900

    :rotating_light: Add temporary SDL2 + OpenGL 3 Bindings.

---
+++ imgui_app.h
@@ -0,0 +1,16 @@
+
+#pragma once
+#include "imgui.h"      // IMGUI_API
+
+#ifndef IMGUI_APP_API
+#define IMGUI_APP_API              IMGUI_API
+#endif
+
+namespace ImGuiApp
+{
+    IMGUI_APP_API bool BeginApplication(const char* name);
+    IMGUI_APP_API void EndApplication();
+
+    IMGUI_APP_API bool BeginFrame();
+    IMGUI_APP_API void EndFrame();
+}
